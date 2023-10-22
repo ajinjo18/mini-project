ee457cd46923c47a440c1a052bfef21d768edf00
ajinjo18 <ajinjo1899@gmail.com>
2023-10-22 12:41:32 +0530

    version 0.4.2

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -1,11 +1,13 @@
 const mongoose=require('mongoose')
+// mongoose.connect('mongodb://127.0.0.1:27017/mini_project')
+const url = 'mongodb+srv://ajinjo1899:Ajinjo@cluster0.3s5dh3f.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(url)
 
-mongoose.connect('mongodb://127.0.0.1:27017/mini_project')
 .then(()=>{
     console.log('mongodb connected');
 })
-.catch(()=>{
-    console.log('connction failed');
+.catch((err)=>{
+    console.log('connction failed',err);
 })
 
 module.exports=mongoose
